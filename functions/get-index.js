6b2cd970cabc1116fccb4911a4e99bb2ea6c5cc5
rafaekhatri11 <rafaekhatri11@gmail.com>
2023-03-09 20:15:08 +0500

    third commit

--- functions/get-index.js
+++ functions/get-index.js
@@ -40,6 +40,49 @@
 
 //   return response
 // }
+// const fs = require("fs")
+// const Mustache = require('mustache')
+// const http = require('axios')
+// const aws4 = require('aws4')
+// const URL = require('url')
+
+// const restaurantsApiRoot = process.env.restaurants_api
+// const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+// const template = fs.readFileSync('static/index.html', 'utf-8')
+
+// const getRestaurants = async () => {
+//   console.log(`loading restaurants from ${restaurantsApiRoot}...`)
+//   const url = URL.parse(restaurantsApiRoot)
+//   const opts = {
+//     host: url.hostname,
+//     path: url.pathname
+//   }
+
+//   aws4.sign(opts)
+
+//   const httpReq = http.get(restaurantsApiRoot, {
+//     headers: opts.headers
+//   })
+//   return (await httpReq).data
+// }
+
+// module.exports.handler = async (event, context) => {
+//   const restaurants = await getRestaurants()
+//   console.log(`found ${restaurants.length} restaurants`)  
+//   const dayOfWeek = days[new Date().getDay()]
+//   const html = Mustache.render(template, { dayOfWeek, restaurants })
+//   const response = {
+//     statusCode: 200,
+//     headers: {
+//       'Content-Type': 'text/html; charset=UTF-8'
+//     },
+//     body: html
+//   }
+
+//   return response
+// }
+
 const fs = require("fs")
 const Mustache = require('mustache')
 const http = require('axios')
@@ -47,6 +90,10 @@ const aws4 = require('aws4')
 const URL = require('url')
 
 const restaurantsApiRoot = process.env.restaurants_api
+const cognitoUserPoolId = process.env.cognito_user_pool_id
+const cognitoClientId = process.env.cognito_client_id
+const awsRegion = process.env.AWS_REGION
+
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
 const template = fs.readFileSync('static/index.html', 'utf-8')
@@ -71,7 +118,15 @@ module.exports.handler = async (event, context) => {
   const restaurants = await getRestaurants()
   console.log(`found ${restaurants.length} restaurants`)  
   const dayOfWeek = days[new Date().getDay()]
-  const html = Mustache.render(template, { dayOfWeek, restaurants })
+  const view = {
+    awsRegion,
+    cognitoUserPoolId,
+    cognitoClientId,
+    dayOfWeek,
+    restaurants,
+    searchUrl: `${restaurantsApiRoot}/search`
+  }
+  const html = Mustache.render(template, view)
   const response = {
     statusCode: 200,
     headers: {
